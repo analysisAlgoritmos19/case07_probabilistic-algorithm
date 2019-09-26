33a22c8f51eb042cf2fd14c2b5ac5acfaa1e7bcd
Esteban Salas <esalasestrada@gmail.com>
2019-09-26 16:21:32 -0600

    Se creo la clase Subgroup y los metodos para rellenar los subgroupos con numeros y letras random, falta probarlo

---
+++ src/probabilistic_balance/Subgroup.java
@@ -0,0 +1,45 @@
+package probabilistic_balance;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Subgroup {
+	
+	private ArrayList<Character> LettersOfSubgroup;
+    private ArrayList<Integer> NumbersOfSubgroup;
+    private int LettersSize = 5;
+    private int NumbersSize = 3;
+    
+    public Subgroup() {
+        LettersOfSubgroup = new ArrayList<>();
+        setLettersOfSubgroup();
+        NumbersOfSubgroup = new ArrayList<>();
+        setNumbersOfSubgroup();
+    }
+    
+    public ArrayList<Character> getLettersOfSubgroup() {
+        return LettersOfSubgroup;
+    }
+
+    public void setLettersOfSubgroup() {
+        Random rand = new Random();
+        char letter;
+        for(int letterIndex=0; letterIndex < LettersSize; letterIndex++){
+           letter =(char) (rand.nextInt((122 - 97) + 1) + 97);
+           this.LettersOfSubgroup.add(letter);
+        }
+
+    }
+
+    public ArrayList<Integer> getNumbersOfSubgroup() {
+        return NumbersOfSubgroup;
+    }
+
+    public void setNumbersOfSubgroup() {
+        Random rand = new Random();
+        for(int numberIndex=0; numberIndex < NumbersSize; numberIndex++){
+            int randNum = rand.nextInt(10);
+            this.NumbersOfSubgroup.add(randNum);
+        }
+    }
+}
