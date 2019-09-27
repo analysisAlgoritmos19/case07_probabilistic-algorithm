d1455e4eaa68c7673ea05a25ff831f4f69285dc1
LuisDavidJv99 <luisdavidjv86@estudiantec.cr>
2019-09-26 20:23:33 -0600

    Se creo la clase Training para empezar a sacar las probabilidades de los subgrupos. Faltan el metodo de verifySubgroup para verificar que algunos subgrupos cumplan con la clave y los metodos de print.

--- src/probabilistic_balance/Training.java
+++ src/probabilistic_balance/Training.java
@@ -1,5 +1,32 @@
 package probabilistic_balance;
 
-public class Training {
+import java.util.ArrayList;
 
+public class Training {
+	
+	int AmountOfSubgroups = 100;
+    ArrayList<Subgroup> SubgroupsForTraining;
+    ArrayList<Subgroup> SubgroupsWithSucceed;
+
+    public Training() {
+        SubgroupsForTraining = new ArrayList<>();
+        setSubgroupsForTraining();
+        SubgroupsWithSucceed = new ArrayList<>();
+    }
+
+
+    public ArrayList<Subgroup> getSubgroupsWithSucceed() {
+        return SubgroupsWithSucceed;
+    }
+
+    public ArrayList<Subgroup> getSubgroupsForTraining() {
+        return SubgroupsForTraining;
+    }
+
+    public void setSubgroupsForTraining() {
+        for (int subgroupIndex=0; subgroupIndex < AmountOfSubgroups; subgroupIndex++){
+            Subgroup subgroup = new Subgroup();
+            this.SubgroupsForTraining.add(subgroup);
+        }
+    }
 }
